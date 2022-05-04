0c12a6818b045d9a796648083f70c8ee20fe3164
huy <phamtranhuy998@gmail.com>
2022-05-04 18:57:47 +0700

    absolute header, update background transparent

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -3,9 +3,14 @@ import Navigation from './Navigation/Navigation'
 
 function Header() {
     return (
-        <header className="layout-max-w flex-between">
-            <div className='text-sm'><span className='text-2xl font-bold'>H</span>Myopic</div>
-            <Navigation />
+        <header className="
+            absolute top-0 inset-x-0 z-50 
+            border-b border-[#ffffff1a]
+            bg-transparent">
+            <div className='layout-max-w flex-between '>
+                <div className='text-sm'><span className='text-2xl font-bold'>H</span>Myopic</div>
+                <Navigation />
+            </div>
         </header>
     )
 }
